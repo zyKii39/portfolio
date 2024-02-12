2cfb0ffdf122a69951df0e0862008fd52ae4587d
zyKii39 <zykii39@gmail.com>
2024-02-12 09:43:10 +0100

    connexion mb4

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>J'AI ENLEVER LE STRICT MODE</p>
+        <p>CONNEXION MB4</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
